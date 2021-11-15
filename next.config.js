8279797c0098c75cc429ac9a515b2dcbea76d04d
Berkhan Eminsoy <9651199+BerkhanEminsoy@users.noreply.github.com>
2021-11-16 01:00:27 +0300

    update config to include sassOptions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    loader: "imgix",
+    path: "https://noop/",
+  },
+}
+
+/*
 module.exports = {
   images: {
     loader: "imgix",
     path: "https://noop/",
   },
 }
+*/
