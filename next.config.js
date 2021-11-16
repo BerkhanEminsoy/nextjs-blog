aba0357a22228bc7688806ae51e6c3e7fb4d7eea
Berkhan Eminsoy <9651199+BerkhanEminsoy@users.noreply.github.com>
2021-11-16 11:53:23 +0300

    remove image loader

--- next.config.js
+++ next.config.js
@@ -5,10 +5,12 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  /*
   images: {
     loader: "imgix",
     path: "https://berkhaneminsoy.github.io/nextjs-blog",
   },
+  */
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 }
